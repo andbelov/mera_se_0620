29f9dcdfaed527c01147563e059df7131f8e0800
Andrey Belov <24381687+andbelov@users.noreply.github.com>
2020-08-12 12:50:38 +0300

    Lecture 10, in progress. Can't waste time on gen the maze

--- Белов/10/PinkFloyd.java
+++ Белов/10/PinkFloyd.java
@@ -4,8 +4,8 @@ import static util.Util10.giveRandom;
 public class PinkFloyd{
 	private final int DX = 0;
 	private final int DY = DX+1;
-	private final int MX = 11; //giveRandomInBound(2, rx);
-	private final int MY = 11; //giveRandomInBound(2, ry);
+	private final int MX = 9; //giveRandomInBound(2, rx);
+	private final int MY = 3; //giveRandomInBound(2, ry);
 	private final int I0 = 0;
 	private final int IX = MX-1;
 	private final int IY = MY-1;
@@ -14,6 +14,9 @@ public class PinkFloyd{
 	private final int entrY;
 	private final int exitX;
 	private final int exitY;
+	private int xC;
+	private int yC;
+	final boolean[][] hasBeen = new boolean[MX][MX];
 	private final StringBuilder sb = new StringBuilder();
 
 	PinkFloyd(){
@@ -27,8 +30,8 @@ public class PinkFloyd{
 		entrY = isEntrOnWallX ? isEntrWallOnZero ? I0 : IY : giveRandom(MY);
 		exitX = isEntrOnWallX ? giveRandom(IX) : !isEntrWallOnZero ? I0 : IX;
 		exitY = isEntrOnWallX ? !isEntrWallOnZero ? I0 : IY : giveRandom(IY);
-		//m[entr[X]][entr[Y]] = false;
-		//m[exit[X]][exit[Y]] = false;
+		m[entrX][entrY][isEntrOnWallX?DX:DY] = false;
+		m[exitX][exitY][isEntrOnWallX?DX:DY] = false;
 		System.out.println("Entrance:["+entrX + "][" + entrY + "], Exit:[" + exitX + "][" + exitY+"]");
 		genPath();
 		printWalls();
@@ -60,14 +63,41 @@ public class PinkFloyd{
 		}
 	}
 	public void genPath(){
-		int x=entrX;
-		int y=entrY;
-		//while(true){
-		System.out.println(":["+x + "][" + y + "]");
-			x += -1 + giveRandom(2) << 1;
-			y += -1 + giveRandom(2) << 1;
-		System.out.println(":["+x + "][" + y + "]");
-		//}
+		xC=entrX;
+		yC=entrY;
+		hasBeen[xC][yC] = true;
+		int xO = xC;
+		int yO = yC;
+		int count = 3;
+		while(0<=--count){
+		//System.out.println(":["+x + "][" + y + "]");
+			final boolean yyR = giveRandom();
+			final boolean stepR = giveRandom();
+			BreakLabel:
+			for(int i=2; --i>=0; ){
+				final boolean yy = (0 == i) ^ yyR;
+				for(int j=2; --j>=0; ){
+					final int step = ((0 == i) ^ stepR) ? 1 : -1;
+					final int xN = yy?xC:xC+step;
+					final int yN = yy?yC+step:yC;
+					if(isOutOfBorder(xN, yN)){
+						continue;
+					}
+					if(!hasBeen[xN][yN]){
+						deegWall(yy,step);
+						xO = xC;
+						yO = yC;
+						xC = xN;
+						yC = yN;
+						break BreakLabel;
+					}
+				}
+			}
+			xC = xO;
+			yC = yO;
+			printWalls();
+		//System.out.println(":["+x + "][" + y + "]");
+		}
 	}
 
 	public void printWalls(){
@@ -99,6 +129,36 @@ public class PinkFloyd{
 			System.out.println(sb);
 		}
 	}
+
+	boolean isOutOfBorder(final int q, final boolean yy){
+		return I0>q || (yy?q>=MY:q>=MX);
+	}
+	boolean isOutOfBorder(final int x, final int y){
+		return I0>x || x>=MX || I0>y || y>=MX;
+	}
+	boolean isWall(final boolean yy, final int step){
+		return m[yy?xC:xC+step][yy?yC+step:yC][yy?DY:DX];
+	}
+	void deegWall(final boolean yy, final int step){
+		m[yy?xC:xC+step][yy?yC+step:yC][yy?DY:DX] = false;
+	}
+	/*void isHasBeen(final boolean yy, final int step){
+		m[yy?xC:xC+step][yy?yC+step:yC][yy?DY:DX] = false;
+	}*/
+
+	boolean isLefWall(final int x, final int y){
+		return m[x][y][DX];
+	}
+	boolean isRitWall(final int x, final int y){
+		return m[x+1][y][DX];
+	}
+	boolean isTopWall(final int x, final int y){
+		return m[x][y][DY];
+	}
+	boolean isBotWall(final int x, final int y){
+		return m[x][y+1][DY];
+	}
+
 /*	boolean isBorder0(final int q){
 		return q == I0;
 	}
@@ -130,11 +190,16 @@ public class PinkFloyd{
 		return isBorder0(q) || m[q-1];
 	}*/
 	char giveCharForPrintMap(final int x, final boolean xx, final int y, final boolean yy){
-		if(xx && yy) return m[x][y][DX]?m[x][y][DY]?'+':'-':m[x][y][DY]?'|':'∙'; //◌ free cell space
-		final boolean l = I0==x ? xx? m[x][y][DX]:false :!yy && m[x-(xx?0:1)][y][DX];
-		final boolean r = IX!=x && !yy && m[x         ][y][DX];
-		final boolean t = I0==y ? yy?m[x][y][DY]:false : !xx && m[x][y-(yy?0:1)][DY];
-		final boolean b = IY!=y && !xx && m[x][y         ][DY];
+		if(xx && yy){
+			if(xC==x && yC==y) return 'C';
+			if(hasBeen[x][y]) return 'b';
+			return m[x][y][DX]?m[x][y][DY]?'+':'-':m[x][y][DY]?'|':'∙'; //◌ free cell space
+		}
+		if(entrX==x && entrY==y) return 'e';
+		final boolean l = I0==x ? xx && m[x][y][DX] : !yy && m[x-(xx?0:1)][y][DX];
+		final boolean r = IX!=x                    && !yy && m[x         ][y][DX];
+		final boolean t = I0==y ? yy && m[x][y][DY] : !xx && m[x][y-(yy?0:1)][DY];
+		final boolean b = IY!=y                    && !xx && m[x]         [y][DY];
 		return l ? r ? t ? b ? '┼' : '┴' : b ? '┬' : '─'
 		             : t ? b ? '┤' : '┘' : b ? '┐' : '→'
 		         : r ? t ? b ? '├' : '└' : b ? '┌' : '←'
@@ -143,9 +208,10 @@ public class PinkFloyd{
 	}
 	char giveCharForPrintMap_(final int x, final boolean xx, final int y, final boolean yy){
 		if(xx && yy) return m[x][y][DX]?m[x][y][DY]?'+':'-':m[x][y][DY]?'|':'∙'; //◌ free cell space
+
 		final boolean l = I0!=x && !yy && m[x-(xx?0:1)][y][DX];
 		final boolean r = IX!=x && !yy && m[x         ][y][DX];
-		final boolean t =  I0==y ? yy?m[x][y][DY]:false : !xx && m[x][y-(yy?0:1)][DY];
+		final boolean t = I0==y ? yy?m[x][y][DY]:false : !xx && m[x][y-(yy?0:1)][DY];
 		final boolean b = IY!=y && !xx && m[x][y         ][DY];
 		return l ? r ? t ? b ? '┼' : '┴' : b ? '┬' : '─'
 		             : t ? b ? '┤' : '┘' : b ? '┐' : '→'
